b39e9e85db3efbbd1642f8230020c11abf0659cb
Sodev34 <111426977+Sodev34@users.noreply.github.com>
2023-06-29 14:14:59 +0100

    Update settings.py

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -8,6 +8,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
+#from django.core.management.utils import get_random_secret_key
 # SECRET_KEY = os.environ.get('SECRET_KEY', default=get_random_secret_key())
 SECRET_KEY = "fp$9^593hsriajg$_%=5trot9g!1qa@ew(o-1#@=&4%=hp46(s"
 
