1099c7039e447ae515e898bb0946884733c6219c
Sodev34 <111426977+Sodev34@users.noreply.github.com>
2023-06-29 14:13:14 +0100

    Update settings.py

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -1,8 +1,5 @@
 import os
 
-#from django.core.management.utils import get_random_secret_key
-
-
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -11,12 +8,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-#SECRET_KEY = os.environ.get('SECRET_KEY', default=get_random_secret_key())
+# SECRET_KEY = os.environ.get('SECRET_KEY', default=get_random_secret_key())
 SECRET_KEY = "fp$9^593hsriajg$_%=5trot9g!1qa@ew(o-1#@=&4%=hp46(s"
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG_VALUE', default=True)
+DEBUG = os.environ.get("DEBUG_VALUE", default=True)
 
 ALLOWED_HOSTS = ["*"]
 
