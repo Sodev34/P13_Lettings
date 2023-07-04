cad88ada09106438f65181f69028a120175f3d94
Sodev34 <111426977+Sodev34@users.noreply.github.com>
2023-07-04 13:59:08 +0100

    Update settings.py

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -10,10 +10,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', default=get_random_secret_key())
+SECRET_KEY = os.environ.get("SECRET_KEY", default=get_random_secret_key())
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG_VALUE', default=True)
+DEBUG = os.environ.get("DEBUG_VALUE", default=True)
 
 ALLOWED_HOSTS = ["*"]
 
