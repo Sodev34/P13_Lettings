2b8468ed757446e6ec79202ebf834552e813e0ff
Sodev34 <111426977+Sodev34@users.noreply.github.com>
2023-07-11 22:49:50 +0100

    sentry

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -1,8 +1,9 @@
-from django.core.management.utils import get_random_secret_key
-
 import os
 import sentry_sdk
 from sentry_sdk.integrations.django import DjangoIntegration
+
+from django.core.management.utils import get_random_secret_key
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
